b97f3333f20431ed7b9d92679e3428f482d95eb6
Shreyansh <shreyanshdk@gmail.com>
2024-08-11 21:13:24 +0530

    Update permutation_string.cpp

--- permutation_string.cpp
+++ permutation_string.cpp
@@ -1,3 +1,20 @@
+// Given a collection of numbers, return all possible permutations.
+
+// Example:
+
+// [1,2,3] will have the following permutations:
+
+// [1,2,3]
+// [1,3,2]
+// [2,1,3] 
+// [2,3,1] 
+// [3,1,2] 
+// [3,2,1]
+// NOTE
+
+// No two entries in the permutation sequence should be the same.
+// For the purpose of this problem, assume that all the numbers in the collection are unique.
+
 void perm(vector<int> &temp, vector<vector<int>> &ans, vector<int> &A, vector<int> &flag)
 {
     if( temp.size() == A.size())
