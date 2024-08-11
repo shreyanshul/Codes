1744778cc7b15b81239e379a35e1278ccf22eb0e
Shreyansh <shreyanshdk@gmail.com>
2024-08-11 21:12:37 +0530

    Update combination_string.cpp

--- combination_string.cpp
+++ combination_string.cpp
@@ -1,3 +1,13 @@
+// Given two integers n and k, return all possible combinations of k numbers out of 1 2 3 ... n.
+
+// Make sure the combinations are sorted.
+
+// To elaborate,
+
+// Within every entry, elements should be sorted. [1, 4] is a valid entry while [4, 1] is not.
+// Entries should be sorted within themselves.
+// Example :
+
 void comb(int start,vector<vector<int>> &ans, vector<int> &temp, int n, int k  )
 {
     for(int i=start; i<=n;i++){
